1551802c6dcad188c943e12ed53a6a6b51a0dee1
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-05-18 21:25:45 +0530

    feat: update routing to include AuthOption, Login, and Signup pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import AuthOption from "./pages/AuthOption";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<AuthOption />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        {/* Redirect unknown routes */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
